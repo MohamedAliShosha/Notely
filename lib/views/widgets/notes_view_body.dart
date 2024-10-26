1cd7b078352e7000d6071bb5128927fd24eea7d4
Mohamed Ali <a74498722@gmail.com>
2024-10-26 14:12:03 -0700

    create Notes List View

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_Bar.dart';
 
 import 'custom_note_item.dart';
+import 'notes_listView.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,12 +12,18 @@ class NotesViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
+        // Takes all the available space too => So I should give a specified height to the listView or it's better to use expand with the listView
+
         children: [
           SizedBox(
             height: 50,
           ),
           CustomAppBar(),
-          NoteItem(),
+          // NoteItem(),
+          Expanded(
+            child:
+                NotesListView(), // Takes all the available space => In this case an exception will be thrown because it is inside a Column &  Column can't have a child that takes all the available space
+          ),
         ],
       ),
     );
