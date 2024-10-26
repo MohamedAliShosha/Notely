feedf056c31a2128f8e01fa7bc790d9e9cd30e1a
Mohamed Ali <a74498722@gmail.com>
2024-10-26 11:26:32 -0700

    create a custom appBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
