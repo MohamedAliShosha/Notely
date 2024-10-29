6dde9a4381211728687f2721282db22866d433a8
Mohamed Ali <a74498722@gmail.com>
2024-10-29 15:04:27 -0700

    create a build yaml file and generating note_model.g.dart file

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,24 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+// I can't use the id of this class to another class
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject // my class extends HiveObject
+
+{
+  // give a unique id to each field
+  // each id is unique per class
+  // I can't use the same id for different fields in the same class
+
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
